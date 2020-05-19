540b6619424e02c72b2f09489f72d6998713448f
Devashish Nayak <devashishnayakrps@gmail.com>
2020-05-19 14:30:36 +0530

    part3

---
+++ build/precache-manifest.aa57259ff399e1215cfd66433364599d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1cbc2873903414a1868415957db17049",
+    "url": "/index.html"
+  },
+  {
+    "revision": "100a53942afdcc92b6b8",
+    "url": "/static/css/main.dd39d8a0.chunk.css"
+  },
+  {
+    "revision": "58bd7345623f9bd76703",
+    "url": "/static/js/2.e3d3a486.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e3d3a486.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "100a53942afdcc92b6b8",
+    "url": "/static/js/main.c7b9c274.chunk.js"
+  },
+  {
+    "revision": "37e42bc47805ba865915",
+    "url": "/static/js/runtime-main.fac6ad05.js"
+  }
+]);

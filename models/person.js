40552f71f44eef0e2a276cfaba75dc3aabcda643
Devashish Nayak <devashishnayakrps@gmail.com>
2020-05-25 20:00:58 +0530

    uibuild

--- models/person.js
+++ models/person.js
@@ -5,36 +5,34 @@ const url = process.env.MONGODB_URI
 
 console.log('connecting to', url)
 
-mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false })
-  .then(result => {
+mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false }).then(() => {
     console.log('connected to MongoDB')
-  })
-  .catch((error) => {
+}).catch((error) => {
     console.log('error connecting to MongoDB:', error.message)
-  })
+})
 
 const personSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: true,
-    uniqueCaseInsensitive: true,
-    minlength: 3
-  },
-  number: {
-    type: String,
-    required: true,
-    minlength: 8
-  }
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        uniqueCaseInsensitive: true,
+        minlength: 3
+    },
+    number: {
+        type: String,
+        required: true,
+        minlength: 8
+    }
 })
 
 personSchema.plugin(uniqueValidator)
 
 personSchema.set('toJSON', {
     transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
     }
 })
 
